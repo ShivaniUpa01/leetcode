6c6d0e4e6a1c1dc34d5ae6754b5f20954b3b4d76
shivani <shivani.01upadhyay@gmail.com>
2022-12-06 14:47:31 +0530

    350. Intersection of Two Arrays II

---
+++ 350-Intersection-of-Two-Arrays-II.py
@@ -0,0 +1,46 @@
+"""Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must
+appear as many times as it shows in both arrays and you may return the result in any order.
+Example 1:
+Input: nums1 = [1,2,2,1], nums2 = [2,2]
+Output: [2,2]
+"""
+
+
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        """Solution 1"""
+        # nums = []
+        # for i in range(len(nums1)):
+        #     for j in range(len(nums2)):
+        #         if nums1[i] == nums2[j]:
+        #             nums.append(nums1[i])
+        #             nums2[j] = -1
+        #             break
+        # return nums
+
+        """Solution 2"""
+        # nums1 = sorted(nums1)
+        # nums2 = sorted(nums2)
+        # check = []
+        # for i in range(len(nums1)):
+        #     for j in range(len(nums2)):
+        #         if nums1[i] == nums2[j]:
+        #             check.append(nums1[i])
+        #             nums2[j] = -1
+        #             break
+        # return check
+
+        """Solution 3"""
+        counter = {}
+        num = []
+        for i in range(len(nums1)):
+            if counter.get(nums1[i]) == None:
+                counter[nums1[i]] = 1
+            else:
+                counter[nums1[i]] += 1
+
+        for item in nums2:
+            if item in counter and counter.get(item) > 0:
+                counter[item] -= 1
+                num.append(item)
+        return num
